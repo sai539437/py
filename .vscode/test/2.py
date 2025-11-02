b0c1a3445f5a52a658757c553b1032ba069ab16d
sai539437 <saiaadyasha720@gmail.com>
2025-11-02 07:24:14 +0000

    upp

---
+++ .vscode/test/2.py
@@ -0,0 +1,9 @@
+#make a list 5 values 2 character 3 numbers if a number is there square it and if a value is there cube it 
+lst=[1,2,3,4,5]
+result=[]
+for i in lst:
+    result.append(i**2)
+print(result)
+result.append(i**3)
+print(result)
+
